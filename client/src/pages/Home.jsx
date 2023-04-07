c82104d1eb243b893ec17675cbdcd4132753c84c
guruvayurappan.m <guruvayurappan.m@zohocorp.com>
2023-04-07 11:06:27 +0530

    Added state from store

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapShot } from 'valtio';
+
+import state from '../store';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapShot(state);
+
   return (
     <div>Home</div>
   )
